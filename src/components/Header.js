7e07ebc0e0af423909629a2d405575d110946949
Marcel Silva <silva.marcel@gmail.com>
2020-01-22 06:47:16 +1100

    feat(tests): add tests to the application

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import Logo from "../img/logo.svg";
 
 const Header = () => (
   <header>
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <a className="navbar-item" href="/">
-          <img width="120" src={Logo} alt=""/>
+          DUPLICA
         </a>
       </div>
     </nav>
