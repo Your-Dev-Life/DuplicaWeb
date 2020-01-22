33afc0d26346c5acee3bc954d330c3df1b36ce3c
Marcel Silva <silva.marcel@gmail.com>
2020-01-22 21:40:14 +1100

    feat(lint): configure lint for react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,24 @@
 module.exports = {
   extends: [
     'airbnb-base',
+    'eslint:recommended',
     'plugin:jest/recommended',
+    'plugin:react/recommended'
   ],
   plugins: [
     'import',
+    'react',
     'jest',
   ],
-  env: {
-    node: true,
-    'jest/globals': true,
-  },
   rules: {
     'import/no-dynamic-require': 0,
     'global-require': 0,
   },
+  settings: {
+    react: {
+      createClass: 'createReactClass',
+      pragma: 'React',
+      version: 'detect'
+    }
+  }
 };
