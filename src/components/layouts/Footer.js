255ccafeee76cab74756d5ee3424f3eb65c5775f
Marcel Silva <silva.marcel@gmail.com>
2020-01-29 06:06:51 +1100

    feat(material-ui): create footer component

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
 
-const Footer = props => (
-  <div><h1>Footer</h1></div>
-);
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    top: 'auto',
+    bottom: 0,
+  },
+  toolbar: {
+    minHeight: 36,
+  },
+}));
+
+const Footer = props => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="fixed" className={classes.appBar} color={"default"}>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant={"body2"} color={"inherit"}>{ props.title || "© Copyright 2020" }</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Footer;
