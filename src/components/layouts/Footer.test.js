e73dd376fe336d89bc6a51ff2a92ad24d0aa780a
Marcel de Jesus Silva <silva.marcel@gmail.com>
2020-01-29 06:10:41 +1100

    [WEB-2] feat(material-ui): create footer component (#9)

--- src/components/layouts/Footer.test.js
+++ src/components/layouts/Footer.test.js
@@ -2,8 +2,17 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Footer from './Footer';
 
-test('renders Footer text in the Footer', () => {
-  const { getByText } = render(<Footer />);
-  const element = getByText(/Footer/i);
-  expect(element).toBeInTheDocument();
+describe('Footer', () => {
+
+  test('renders \'© Copyright 2020\' text when no title is passed', () => {
+    const { getByText } = render(<Footer />);
+    const element = getByText(/© Copyright 2020/i);
+    expect(element).toBeInTheDocument();
+  });
+
+  test('renders Anything text when Anything is passed as the title', () => {
+    const { getByText } = render(<Footer title="Anything" />);
+    const element = getByText(/Anything/i);
+    expect(element).toBeInTheDocument();
+  });
 });
