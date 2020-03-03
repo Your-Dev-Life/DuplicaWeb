e73bab35d7b5facdd2ac102e1b89ae22861890bd
Marcel Silva <silva.marcel@gmail.com>
2020-03-04 06:54:07 +1100

    test(login): WIP - adding more tests for login

--- src/components/login/index.test.js
+++ src/components/login/index.test.js
@@ -19,4 +19,14 @@ describe('Login', () => {
     expect(elementUsername).toBeInTheDocument();
     expect(elementPassword).toBeInTheDocument();
   });
+
+  test('should show message \'Username is required\' when username is not informed', () => {
+    const auth = authentication();
+    const { queryByText } = renderComponent(auth);
+    const elementSignInButton = queryByText(/Sign in/i);
+    elementSignInButton.click();
+    const elementUsernameIsRequired = queryByText(/Username is required/i);
+    expect(elementSignInButton).toBeInTheDocument();
+    expect(elementUsernameIsRequired).toBeInTheDocument();
+  });
 });
