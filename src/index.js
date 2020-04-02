ca06b39a157affbc20f31bbfe713a3adf967c62d
Marcel Silva <silva.marcel@gmail.com>
2020-04-02 16:58:57 +1100

    refactor(config): add reload support for s3

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import { render } from 'react-dom';
 import App from './components/app';
 import api from './api';
 import './i18n/i18n';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
+
+const path = (/#!(\/.*)$/.exec(location.hash) || [])[1];
+if (path) {
+  history.replace(path);
+}
 
 render(<App api={api} />, document.getElementById('app'));
