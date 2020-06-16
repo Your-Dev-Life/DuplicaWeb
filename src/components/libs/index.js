f3fe2cfca63adf27cda7f0829a89d1e65bef7777
marcel.silva <marcel.silva@service.nsw.gov.au>
2020-06-16 14:55:46 +1000

    refactor(structure): updated the files and folder structure for Address and Contact

--- src/components/libs/index.js
+++ src/components/libs/index.js
@@ -1,6 +1,10 @@
 import { FormDialog, FormFooter } from './form';
+import { Address } from './address';
+import { Contact } from './contact';
 
 export {
   FormDialog,
   FormFooter,
+  Address,
+  Contact,
 };
