6ed64d9db2d2cb030e90ff6ad26768b07ca4785a
marcel.silva <marcel.silva@service.nsw.gov.au>
2020-06-16 14:53:20 +1000

    test(coverage): remove mocks from the coverage

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
     'src/components/routes.js',
     'src/api/index.js',
     'src/i18n/',
+    '.mock.js',
   ],
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/mocks/fileMock.js',
